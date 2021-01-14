8260340d2e3a7397db049ee9ba97f36eff7fb3ac
Hitominakudo <hansenquadros@gmail.com>
2021-01-15 03:28:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post('/',(req,res)=>{
     intentMap.set('DOB',demo3);
     intentMap.set('City',demo4);
     intentMap.set('Country',demo5);
-    intentMap.set('Aboutme',demo6);
+    intentMap.set('AboutMe',demo6);
     intentMap.set('Friends',demo7);
     agent.handleRequest(intentMap);
 
